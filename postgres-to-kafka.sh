ef3fd2ce053e74ef4fc2f0f69f09cf7ffccdf0d9
Agung Raka Wibawa <77326619+git-raka@users.noreply.github.com>
2022-11-23 16:23:51 +0700

    Create postgres-to-kafka.sh

---
+++ postgres-to-kafka.sh
@@ -0,0 +1,20 @@
+curl -H 'Content-Type: application/json' 192.168.18.75:8083/connectors --data '
+ {
+    "name": "postgresql",
+    "config": {
+        "connector.class": "io.debezium.connector.postgresql.PostgresConnector",
+        "tasks.max": "1",
+        "plugin.name": "pgoutput",
+        "database.hostname": "192.168.18.75",
+        "database.port": "5432",
+        "database.user": "postgres",
+        "database.password": "postgres",
+        "database.dbname": "postgres",
+        "database.server.name": "postgres",
+        "key.converter": "org.apache.kafka.connect.json.JsonConverter",
+        "value.converter": "org.apache.kafka.connect.json.JsonConverter",
+        "key.converter.schemas.enable": "false",
+        "value.converter.schemas.enable": "false",
+        "snapshot.mode": "always"
+    }
+}'
